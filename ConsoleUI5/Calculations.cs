cf5d1fe65766fdc39774e162a03e57304802db51
boodscode237 <boodscode@gmail.com>
2022-11-29 01:43:01 +0500

    Add project files.

---
+++ ConsoleUI5/Calculations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maths.Add
+{
+    public static class Calculations
+    {
+        public static double Add(double x, double y)
+        {
+            Console.WriteLine(x + y);
+            return x + y;
+        }
+    }
+}
---
+++ ConsoleUI5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Maths.Add;
+
+namespace ConsoleUI5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<PersonModel> people = new List<PersonModel>();
+            Calculations.Add(5, 6);
+            PersonModel person = new PersonModel();
+            Console.ReadLine();
+        }
+    }
+}
